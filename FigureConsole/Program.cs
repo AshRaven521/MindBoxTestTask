6d38c270c47d2ff86fe663a43f6733a7a7ad7d2a
AshRaven521 <ivan.kondrashov.92@mail.ru>
2023-02-09 23:41:49 +0300

    add console project for examples

---
+++ FigureConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FigureLibrary;
+
+namespace FigureConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle
+            {
+                Radius = 5.0
+            };
+            Console.WriteLine($"Circle area: {circle.Area()}");
+
+            Triangle triangle = new Triangle
+            {
+                A = 60.0,
+                B = 91.0,
+                C = 109.0
+            };
+
+            Console.WriteLine($"Triangle area: {triangle.Area()}");
+        }
+    }
+}
